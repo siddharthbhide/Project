bca6cd56291fa7b122b493f9a148becd56052f40
siddharthbhide28 <siddharthbhide28@gmail.com>
2018-11-28 19:38:00 +0530

    Add Login Screen Send button

---
+++ MyApplication/app/src/main/java/com/example/siddharth/myapplication/ExamOptionActivity.java
@@ -0,0 +1,13 @@
+package com.example.siddharth.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamOptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_option);
+    }
+}
