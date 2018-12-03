92080645639d1c4e197fc492f9b3d9d86b8a8e2b
siddharthbhide28 <siddharthbhide28@gmail.com>
2018-12-03 21:22:26 +0530

    Add Online Exam Screen

--- MyApplication/app/src/main/java/com/example/siddharth/myapplication/ExamOptionActivity.java
+++ MyApplication/app/src/main/java/com/example/siddharth/myapplication/ExamOptionActivity.java
@@ -1,7 +1,9 @@
 package com.example.siddharth.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ExamOptionActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class ExamOptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_option);
+    }
+
+    public void OnClickRegularExam(View view)
+    {
+        Intent objExample = new Intent(ExamOptionActivity.this,ExamActivity.class);
+        startActivity(objExample);
+    }
+
+    public void OnClickCompetitionExam(View view)
+    {
+    }
+
+    public void OnClickHomeWork(View view)
+    {
     }
 }
