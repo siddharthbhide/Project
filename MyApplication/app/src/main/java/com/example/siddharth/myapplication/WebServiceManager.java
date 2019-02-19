c5c078b55011382dfb858bc48bcde1bc7ed0e845
siddharthbhide28 <siddharthbhide28@gmail.com>
2019-02-19 17:17:53 +0530

    Create WebServiceManager Singletone.

--- MyApplication/app/src/main/java/com/example/siddharth/myapplication/WebServiceManager.java
+++ MyApplication/app/src/main/java/com/example/siddharth/myapplication/WebServiceManager.java
@@ -1,14 +1,15 @@
 package com.example.siddharth.myapplication;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,25 +20,62 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.android.volley.VolleyLog.TAG;
+
 public class WebServiceManager {
 
+    public static WebServiceManager objWebServiceManagerInstance = null;
     StringRequest objStringRequest;
-    RequestQueue objRequestQueue;
+    private RequestQueue objRequestQueue;
     Context objContext;
     StudentDetails objStudentDetails;
     Exam_Details objExam_Details;
     ExamQuestions objExamQuestions;
-    List<ExamQuestions> listOfExamQuestions= new ArrayList();
+    List<ExamQuestions> listOfExamQuestions = new ArrayList();
+    String strResult;
+    JSONObject jsonobject = null;
 
-    public  WebServiceManager(Context context){
+    private WebServiceManager(Context context) {
         objContext = context;
+        objRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized WebServiceManager getInstance(Context context) {
+        if (objWebServiceManagerInstance == null) {
+            objWebServiceManagerInstance = new WebServiceManager(context);
+        }
+        return objWebServiceManagerInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (objRequestQueue == null) {
+            objRequestQueue = Volley.newRequestQueue(objContext.getApplicationContext());
+        }
+        return objRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request, String strTag) {
+        request.setTag(TextUtils.isEmpty(strTag) ? TAG : strTag);
+        getRequestQueue().add(request);
+    }
+
+    public <T> void addToRequestQueue(Request<T> request) {
+        request.setTag(TAG);
+        getRequestQueue().add(request);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (objRequestQueue != null) {
+            objRequestQueue.cancelAll(tag);
+        }
     }
 
 
-    public void getLoginStudentDetails(String strUrl,final String strUserName,final String strPassword){
 
-        objRequestQueue = VolleySingleton.getInstance(objContext).getRequestQueue();
-        objStringRequest = new StringRequest(Request.Method.POST,strUrl,
+    public void getLoginStudentDetails(String strUrl, final String strUserName, final String strPassword) {
+
+        objRequestQueue = WebServiceManager.getInstance(objContext).getRequestQueue();
+        objStringRequest = new StringRequest(Request.Method.POST, strUrl,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -45,7 +83,7 @@ public class WebServiceManager {
                             JSONArray jsonResponse = new JSONArray(response);
                             CheckStudentLoginDetails(jsonResponse);
 
-                        }catch (JSONException e){
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -68,46 +106,43 @@ public class WebServiceManager {
         objRequestQueue.add(objStringRequest);
     }
 
-    private void CheckStudentLoginDetails(JSONArray jsonResponse){
+    private void CheckStudentLoginDetails(JSONArray jsonResponse) {
 
+        jsonobject = null;
+        strResult = "";
         try {
-
-            if (jsonResponse.length() == 1) {
-                return;
-            } else {
-                for (int nIndex = 0; nIndex < jsonResponse.length(); nIndex++) {
-                    JSONObject jsonobject = jsonResponse.getJSONObject(nIndex);
-
-                    objStudentDetails.setId(jsonobject.getString("id"));
-                    objStudentDetails.setUserName(jsonobject.getString("name"));
-                    objStudentDetails.setDateOfBirth(jsonobject.getString("dob"));
-                    objStudentDetails.setAdmissionDate(jsonobject.getString("admissionDate"));
-                    objStudentDetails.setDateAdded(jsonobject.getString("dateAdded"));
-                    objStudentDetails.setFranId(jsonobject.getString("franId"));
-                    objStudentDetails.setEmail(jsonobject.getString("email"));
-                    objStudentDetails.setCourseLevel(jsonobject.getString("courseLevel"));
-                    objStudentDetails.setCourseId(jsonobject.getString("courseId"));
-                    objStudentDetails.setStd(jsonobject.getString("std"));
-                    objStudentDetails.setSchool(jsonobject.getString("school"));
-                    objStudentDetails.setMobileNo(jsonobject.getString("mobileNo"));
-                    objStudentDetails.setMotherOccupation(jsonobject.getString("motherOccupation"));
-                    objStudentDetails.setFatherOccupation(jsonobject.getString("fatherOccupation"));
-                    objStudentDetails.setMotherName(jsonobject.getString("motherName"));
-                    objStudentDetails.setAddress(jsonobject.getString("address"));
-                    objStudentDetails.setFatherName(jsonobject.getString("fatherName"));
-                    objStudentDetails.setSex(jsonobject.getString("sex"));
-
-                }
+            jsonobject = jsonResponse.getJSONObject(0);
+            strResult = jsonobject.getString("result");
+            if (0 == strResult.compareToIgnoreCase("1")) {
+                objStudentDetails.setId(jsonobject.getString("id"));
+                objStudentDetails.setUserName(jsonobject.getString("name"));
+                objStudentDetails.setDateOfBirth(jsonobject.getString("dob"));
+                objStudentDetails.setAdmissionDate(jsonobject.getString("admissionDate"));
+                objStudentDetails.setDateAdded(jsonobject.getString("dateAdded"));
+                objStudentDetails.setFranId(jsonobject.getString("franId"));
+                objStudentDetails.setEmail(jsonobject.getString("email"));
+                objStudentDetails.setCourseLevel(jsonobject.getString("courseLevel"));
+                objStudentDetails.setCourseId(jsonobject.getString("courseId"));
+                objStudentDetails.setStd(jsonobject.getString("std"));
+                objStudentDetails.setSchool(jsonobject.getString("school"));
+                objStudentDetails.setMobileNo(jsonobject.getString("mobileNo"));
+                objStudentDetails.setMotherOccupation(jsonobject.getString("motherOccupation"));
+                objStudentDetails.setFatherOccupation(jsonobject.getString("fatherOccupation"));
+                objStudentDetails.setMotherName(jsonobject.getString("motherName"));
+                objStudentDetails.setAddress(jsonobject.getString("address"));
+                objStudentDetails.setFatherName(jsonobject.getString("fatherName"));
+                objStudentDetails.setSex(jsonobject.getString("sex"));
             }
-        }catch (Exception objException){
+
+        } catch (Exception objException) {
             objException.printStackTrace();
         }
     }
 
-    public void getExamDetails(String strUrl,final String strExam_Code){
+    public void getExamDetails(String strUrl, final String strExam_Code) {
 
-        objRequestQueue = VolleySingleton.getInstance(objContext).getRequestQueue();
-        objStringRequest = new StringRequest(Request.Method.POST,strUrl,
+        objRequestQueue = WebServiceManager.getInstance(objContext).getRequestQueue();
+        objStringRequest = new StringRequest(Request.Method.POST, strUrl,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -115,7 +150,7 @@ public class WebServiceManager {
                             JSONArray jsonResponse = new JSONArray(response);
                             CheckExam_CodeDetails(jsonResponse);
 
-                        }catch (JSONException e){
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -139,33 +174,33 @@ public class WebServiceManager {
 
     private void CheckExam_CodeDetails(JSONArray jsonResponse) {
 
+        jsonobject = null;
+        strResult = "";
         try {
-
-            if (jsonResponse.length() == 1) {
-                return;
-            } else {
-                for (int nIndex = 0; nIndex < jsonResponse.length(); nIndex++) {
-                    JSONObject jsonobject = jsonResponse.getJSONObject(nIndex);
-
-                    objExam_Details.setId(jsonobject.getString("id"));
-                    objExam_Details.setName(jsonobject.getString("name"));
-                    objExam_Details.setCourse(jsonobject.getString("course"));
-                    objExam_Details.setLevel(jsonobject.getString("level"));
-                    objExam_Details.setType(jsonobject.getString("type"));
-                    objExam_Details.setStartDate(jsonobject.getString("startDate"));
-                    objExam_Details.setEndDate(jsonobject.getString("endDate"));
-                    objExam_Details.setIsCompleted(jsonobject.getString("isCompleted"));
-                }
+            jsonobject = jsonResponse.getJSONObject(0);
+            strResult = jsonobject.getString("result");
+            if (0 == strResult.compareToIgnoreCase("1")) {
+                objExam_Details.setId(jsonobject.getString("id"));
+                objExam_Details.setName(jsonobject.getString("name"));
+                objExam_Details.setCourse(jsonobject.getString("course"));
+                objExam_Details.setLevel(jsonobject.getString("level"));
+                objExam_Details.setType(jsonobject.getString("type"));
+                objExam_Details.setStartDate(jsonobject.getString("startDate"));
+                objExam_Details.setEndDate(jsonobject.getString("endDate"));
+                objExam_Details.setIsCompleted(jsonobject.getString("isCompleted"));
             }
-        }catch (Exception objException){
+        } catch (Exception objException)
+
+        {
             objException.printStackTrace();
         }
+
     }
 
-    public void getExamQuestion(String strUrl,final String strExamId){
+    public void getExamQuestion(String strUrl, final String strExamId) {
 
-        objRequestQueue = VolleySingleton.getInstance(objContext).getRequestQueue();
-        objStringRequest = new StringRequest(Request.Method.POST,strUrl,
+        objRequestQueue = WebServiceManager.getInstance(objContext).getRequestQueue();
+        objStringRequest = new StringRequest(Request.Method.POST, strUrl,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -173,7 +208,7 @@ public class WebServiceManager {
                             JSONArray jsonResponse = new JSONArray(response);
                             CheckExamQuestion(jsonResponse);
 
-                        }catch (JSONException e){
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -197,27 +232,26 @@ public class WebServiceManager {
 
     private void CheckExamQuestion(JSONArray jsonResponse) {
 
+        jsonobject = null;
+        strResult = "";
+
         try {
+            for (int nIndex = 0; nIndex < jsonResponse.length(); nIndex++) {
 
-            if (jsonResponse.length() == 1) {
-                return;
-            } else {
-                for (int nIndex = 0; nIndex < jsonResponse.length(); nIndex++) {
-                    JSONObject jsonobject = jsonResponse.getJSONObject(nIndex);
+                jsonobject = jsonResponse.getJSONObject(nIndex);
+                objExamQuestions.setId(jsonobject.getString("id"));
+                objExamQuestions.setDescription(jsonobject.getString("description"));
+                objExamQuestions.setOptionA(jsonobject.getString("optionA"));
+                objExamQuestions.setOptionB(jsonobject.getString("optionB"));
+                objExamQuestions.setOptionC(jsonobject.getString("optionC"));
+                objExamQuestions.setOptionD(jsonobject.getString("optionD"));
+                objExamQuestions.setCourse(jsonobject.getString("course"));
+                objExamQuestions.setAnswer(jsonobject.getString("answer"));
 
-                    objExamQuestions.setId(jsonobject.getString("id"));
-                    objExamQuestions.setDescription(jsonobject.getString("description"));
-                    objExamQuestions.setOptionA(jsonobject.getString("optionA"));
-                    objExamQuestions.setOptionB(jsonobject.getString("optionB"));
-                    objExamQuestions.setOptionC(jsonobject.getString("optionC"));
-                    objExamQuestions.setOptionD(jsonobject.getString("optionD"));
-                    objExamQuestions.setCourse(jsonobject.getString("course"));
-                    objExamQuestions.setAnswer(jsonobject.getString("answer"));
-
-                    listOfExamQuestions.add(objExamQuestions);
-                }
+                listOfExamQuestions.add(objExamQuestions);
             }
-        }catch (Exception objException){
+
+        } catch (Exception objException) {
             objException.printStackTrace();
         }
     }
