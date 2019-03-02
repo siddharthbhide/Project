f69292c78983e37f507643d348545f95c6dc2bfe
rajesh <smart.ios.developer@gmail.com>
2019-03-02 16:11:26 +0530

    added few new xml files

---
+++ MyApplication/app/src/main/java/com/example/siddharth/myapplication/NavigationListener.java
@@ -0,0 +1,69 @@
+package com.example.siddharth.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+
+public class NavigationListener implements NavigationView.OnNavigationItemSelectedListener
+{
+    private Context cntx = null;
+    private Activity activity = null;
+
+    public NavigationListener(Activity activity)
+    {
+        this.cntx = activity.getApplicationContext();
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+        Intent objIntent = null;
+
+        if (id == R.id.nav_gotowebsite) {
+
+            objIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://csdfoundation.co.in/index.php"));
+            cntx.startActivity(objIntent);
+        }
+        else if (id == R.id.nav_gallery) {
+            objIntent = new Intent(this.activity, AboutUsActivity.class);
+            cntx.startActivity(objIntent);
+        }
+        else if (id == R.id.nav_level_exam) {
+            objIntent = new Intent(this.activity, ExamOptionActivity.class);
+            cntx.startActivity(objIntent);
+        }
+        else if (id == R.id.nav_practice_paper) {
+            objIntent = new Intent(this.activity, ExamOptionActivity.class);
+            cntx.startActivity(objIntent);
+        }
+        else if (id == R.id.nav_report_history) {
+            objIntent = new Intent(this.activity, ReportHistoryActivity.class);
+            cntx.startActivity(objIntent);
+        }
+        else if (id == R.id.nav_rule) {
+            objIntent = new Intent(this.activity, RulesActivity.class);
+            cntx.startActivity(objIntent);
+        }
+        else if (id == R.id.nav_result) {
+            objIntent = new Intent(this.activity, RegistrationFormActivity.class);
+            cntx.startActivity(objIntent);
+        }
+        else if (menuItem.getTitle().equals("Contact Us")) {
+            objIntent = new Intent(this.activity, ContactUsActivity.class);
+            cntx.startActivity(objIntent);
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/siddharth/myapplication/RulesActivity.java
@@ -0,0 +1,35 @@
+package com.example.siddharth.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class RulesActivity extends AppCompatActivity {
+
+    NavigationListener navigationLitener = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+        this.setTitle(R.string.rule);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        this.navigationLitener = new NavigationListener(this);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this.navigationLitener);
+    }
+
+}
